bf836ab7dabea73849521090c66e16c700e6a3a6
Cbyadav007 <jaikrr10@gmail.com>
2022-06-04 21:39:23 +0530

    E2

---
+++ web-18-c-2-template_249873/scripts/checkout.js
@@ -0,0 +1,27 @@
+document.querySelector("#confirm").addEventListener("click",checkFun);
+var count=0;
+function checkFun()
+{
+    alert("Your order is accepted");
+   setTimeout(function(){
+        alert("Your order is being Prepared");
+       
+    },3000);
+
+    setTimeout(function(){
+        alert("Your order is being Packed");
+        
+    },8000);
+    setTimeout(function(){
+        alert("Your order is out for delivery");
+       
+    },10000);
+
+    setTimeout(function(){
+        alert("Order delivered");
+        
+    },12000);
+    
+
+
+}
